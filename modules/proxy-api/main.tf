f4c84fabdbfca6d21b313dd843d35297b6fbedc2
Tom Arne <tom.brandvold@vy.no>
2026-05-18 09:35:46 +0200

    Add default variable for method authorization

--- modules/proxy-api/main.tf
+++ modules/proxy-api/main.tf
@@ -8,7 +8,7 @@ resource "aws_api_gateway_method" "rest_service_any" {
   rest_api_id   = var.rest_api_id
   resource_id   = aws_api_gateway_resource.rest_service_proxy.id
   http_method   = "ANY"
-  authorization = "NONE"
+  authorization = var.method_authorization
 
   request_parameters = {
     "method.request.path.proxy"  = true
--- modules/proxy-api/variables.tf
+++ modules/proxy-api/variables.tf
@@ -16,6 +16,18 @@
 variable "rest_api_id" {
     type = string
 }
+
 variable "parent_id" {
     type = string
+}
+
+variable "method_authorization" {
+    type = string
+    default = "AWS_IAM"
+    description = "Type of authorization used for the method (NONE, CUSTOM, AWS_IAM, COGNITO_USER_POOLS)"
+
+    validation {
+        condition     = contains(["NONE", "CUSTOM", "AWS_IAM", "COGNITO_USER_POOLS"], var.method_authorization)
+        error_message = "endpoint_type must be one of NONE, CUSTOM, AWS_IAM or COGNITO_USER_POOLS."
+    }
 }
