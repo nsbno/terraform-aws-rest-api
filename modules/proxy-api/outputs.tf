d10ff3f594b993763d73be4cfc6f99e843e2f0c7
Tom Arne <tom.brandvold@vy.no>
2026-05-27 14:41:25 +0200

    Remove commented-out code

--- modules/proxy-api/outputs.tf
+++ modules/proxy-api/outputs.tf
@@ -1,8 +1,3 @@
-# output "rest_api_id" {
-#   description = "The ID of the REST API."
-#   value       = aws_api_gateway_rest_api.rest_service.id
-# }
-
 output "recource_proxy_id" {
     value = aws_api_gateway_resource.rest_service_proxy.id
 }
@@ -10,16 +5,3 @@ output "recource_proxy_id" {
 output "http_method" {
     value = aws_api_gateway_method.rest_service_any.http_method
 }
-
-# output "api_gateway_stage_name" {
-#     value = module.rest_api.api_gateway_stage_name
-# }
-
-# output "api_gateway_stage_arn" {
-#     value = module.rest_api.api_gateway_stage_arn
-# }
-
-
-
-
- 
--- modules/proxy-api/variables.tf
+++ modules/proxy-api/variables.tf
@@ -1,18 +1,3 @@
-# variable "name" {
-#   description = "The name of the API Gateway REST API."
-#   type        = string
-# }
-
-# variable "redeployment_triggers" {
-#   description = "Additional triggers passed in from the caller, to be merged with this module's own triggers."
-#   type        = map(string)
-#   default     = {}
-# }
-
-# variable "hosted_zone_name" {}
-
-# variable "alb_arn" {}
-
 variable "rest_api_id" {
     type = string
 }
