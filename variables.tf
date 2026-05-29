b5a7fa78cefecbf476865d5f2103e4972c54727f
Tom Arne <tom.brandvold@vy.no>
2026-05-29 13:24:17 +0200

    Fix missing closing brace for endpoint_type variable in variables.tf

--- variables.tf
+++ variables.tf
@@ -11,7 +11,7 @@ variable "endpoint_type" {
     condition     = contains(["REGIONAL", "EDGE", "PRIVATE"], var.endpoint_type)
     error_message = "endpoint_type must be one of REGIONAL, EDGE, or PRIVATE."
   }
-  
+}
 variable "openapi_body" {
   type     = string
   nullable = true
