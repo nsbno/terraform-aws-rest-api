91fc7c6dade90a0eb97f76a074ff27f338a44277
Tom Arne <tom.brandvold@vy.no>
2026-05-29 13:07:25 +0200

    Refactor outputs and resource names to use 'this' reference for consistency

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "rest_api_id" {
   description = "The ID of the REST API."
-  value       = aws_api_gateway_rest_api.rest_service.id
+  value       = aws_api_gateway_rest_api.this.id
 }
 
 output "root_resource_id" {
   description = "The root resource ID of the REST API."
-  value       = aws_api_gateway_rest_api.rest_service.root_resource_id
+  value       = aws_api_gateway_rest_api.this.root_resource_id
 }
 
 output "stage_name" {
-  value = aws_api_gateway_stage.rest_service.stage_name
+  value = aws_api_gateway_stage.this.stage_name
 }
 
 output "stage_arn" {
-  value = aws_api_gateway_stage.rest_service.arn
+  value = aws_api_gateway_stage.this.arn
 }
